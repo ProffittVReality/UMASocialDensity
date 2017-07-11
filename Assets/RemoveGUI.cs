5f3a83417d6ea12480380a8b681d738bd87e5d5e
Ryan <uvaperceptionlab@gmail.com>
2017-07-11 14:59:01 -0400

    new sound added

---
+++ Assets/RemoveGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveGUI : MonoBehaviour {
+	public KeyCode method;
+	public GameObject target;
+	//public bool switcher;
+
+
+	void Awake() {
+		//switcher = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(method))
+			//Debug.Log("key was pressed");
+			target.SetActive(false); 
+
+
+		//switcher = true;
+		//if (switcher == true)
+
+	}
+}

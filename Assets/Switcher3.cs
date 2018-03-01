3c57540e55323dd1ed6a6af47d2af1420325d0b2
ProffittVReality <uvaperceptionlab@gmail.com>
2018-02-28 20:18:43 -0500

    removing code

--- Assets/Switcher3.cs
+++ Assets/Switcher3.cs
@@ -7,24 +7,15 @@ public class Switcher3 : MonoBehaviour {
 	public GameObject target2;
 	public bool dense = false;
 	public bool sparse = false;
-	public GameObject cam;
-	//public Text timer;
-	private float timeLeft = 100.0f;
 
 	void Awake() {
 		//switcher = false;
-		cam = GameObject.FindGameObjectWithTag("CameraRig");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print (timeLeft);
-		/*if (Input.GetKeyDown(method))
-			//Debug.Log("key was pressed");
-			target.SetActive(true); 
-		if (target.active)
-			target2.SetActive(false);
-			*/
+
 		if (dense) {
 			target.SetActive (true);
 			target2.SetActive (false);
@@ -33,14 +24,7 @@ public class Switcher3 : MonoBehaviour {
 			target2.SetActive (true);
 			target.SetActive (false);
 		}
-		timeLeft -= Time.deltaTime;
-		if (timeLeft < 0) {
-			//target.SetActive (false);
-			//target2.SetActive (false);
-			cam.SetActive (false);
-		}
-			//switcher = true;
-		//if (switcher == true)
+
 	}
 	void setDense(){
 		dense = true;

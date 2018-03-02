6a6293be47fac047de77112abf606089ff4cc9c5
ProffittVReality <uvaperceptionlab@gmail.com>
2018-03-01 23:52:36 -0500

    edited merge conflict

--- Assets/Switcher3.cs
+++ Assets/Switcher3.cs
@@ -6,7 +6,6 @@ public class Switcher3 : MonoBehaviour {
 	public KeyCode method;
 	public GameObject target;
 	public GameObject target2;
-<<<<<<< HEAD
 	public bool dense = false;
 	public bool sparse = false;
 	public bool start = false;
@@ -19,18 +18,12 @@ public class Switcher3 : MonoBehaviour {
 		//switcher = false;
 		count = 0;
 		c = blackBlock.GetComponent<Renderer> ().material.color;
-=======
-	//public bool switcher;
-
-
-	void Awake() {
-		//switcher = false;
->>>>>>> parent of 898e409... outdoor scene update
+		blackBlock.SetActive (false);
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
 		if (start) {
 			if (count == 0) {
 				timeLeft = float.Parse (timer.text) * 60.0f;
@@ -46,6 +39,9 @@ public class Switcher3 : MonoBehaviour {
 			}
 
 			timeLeft -= Time.deltaTime;
+			if (timeLeft < 6 && timeLeft > 5) {
+				blackBlock.SetActive (true);
+			}
 			if (timeLeft <= 5) {
 				c.a += Mathf.Clamp01 (Time.deltaTime / 5);
 				blackBlock.GetComponent<Renderer> ().material.color = c;
@@ -62,18 +58,11 @@ public class Switcher3 : MonoBehaviour {
 	}
 	void setSparse(){
 		sparse = true;
-=======
 		if (Input.GetKeyDown(method))
 			//Debug.Log("key was pressed");
 			target.SetActive(true); 
 		if (target.active)
 			target2.SetActive(false);
-
-
-			//switcher = true;
-		//if (switcher == true)
-			
->>>>>>> parent of 898e409... outdoor scene update
 	}
 	void exportStart(){
 		start = true;

b587a1533019d5c5e4cf239c9c75685b803aac09
ProffittVReality <uvaperceptionlab@gmail.com>
2018-03-02 02:11:57 -0500

    Revert "outdoor scene update"
    This reverts commit 898e40969a6ecf6e2793079182714bab625e0b1c.

--- Assets/Switcher3.cs
+++ Assets/Switcher3.cs
@@ -8,6 +8,7 @@ public class Switcher3 : MonoBehaviour {
 	public GameObject target;
 	public GameObject target2;
 <<<<<<< HEAD
+<<<<<<< HEAD
 	public bool dense = false;
 	public bool sparse = false;
 	public bool start = false;
@@ -23,16 +24,22 @@ public class Switcher3 : MonoBehaviour {
 		blackBlock.SetActive (false);
 			
 =======
+=======
+>>>>>>> parent of 898e409... outdoor scene update
 	//public bool switcher;
 
 
 	void Awake() {
 		//switcher = false;
+<<<<<<< HEAD
+>>>>>>> parent of 898e409... outdoor scene update
+=======
 >>>>>>> parent of 898e409... outdoor scene update
 	}
 	
 	// Update is called once per frame
 	void Update () {
+<<<<<<< HEAD
 <<<<<<< HEAD
 		if (start) {
 			if (count == 0) {
@@ -70,11 +77,14 @@ public class Switcher3 : MonoBehaviour {
 		sparse = true;
 =======
 >>>>>>> parent of 898e409... outdoor scene update
+=======
+>>>>>>> parent of 898e409... outdoor scene update
 		if (Input.GetKeyDown(method))
 			//Debug.Log("key was pressed");
 			target.SetActive(true); 
 		if (target.active)
 			target2.SetActive(false);
+<<<<<<< HEAD
 <<<<<<< HEAD
 	}
 	void exportStart(){
@@ -151,11 +161,16 @@ public class Switcher3 : MonoBehaviour {
 	void exportStart(){
 		start = true;
 =======
+=======
+>>>>>>> parent of 898e409... outdoor scene update
 
 
 			//switcher = true;
 		//if (switcher == true)
 			
+<<<<<<< HEAD
+>>>>>>> parent of 898e409... outdoor scene update
+=======
 >>>>>>> parent of 898e409... outdoor scene update
 	}
 }

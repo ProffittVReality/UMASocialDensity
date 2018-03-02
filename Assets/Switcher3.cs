8f5248b9ef1178d725a608e76c7db97671e6bfd0
ProffittVReality <uvaperceptionlab@gmail.com>
2018-03-02 03:51:39 -0500

    Update Switcher3.cs

--- Assets/Switcher3.cs
+++ Assets/Switcher3.cs
@@ -1,57 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
-using System.Collections;
-
-public class Switcher3 : MonoBehaviour {
-	public KeyCode method;
-	public GameObject target;
-	public GameObject target2;
-	public bool dense = false;
-	public bool sparse = false;
-	public GameObject cam;
-	//public Text timer;
-	private float timeLeft = 100.0f;
-
-	void Awake() {
-		//switcher = false;
-		cam = GameObject.FindGameObjectWithTag("CameraRig");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		print (timeLeft);
-		/*if (Input.GetKeyDown(method))
-			//Debug.Log("key was pressed");
-			target.SetActive(true); 
-		if (target.active)
-			target2.SetActive(false);
-			*/
-		if (dense) {
-			target.SetActive (true);
-			target2.SetActive (false);
-		} 
-		else if (sparse){
-			target2.SetActive (true);
-			target.SetActive (false);
-		}
-		timeLeft -= Time.deltaTime;
-		if (timeLeft < 0) {
-			//target.SetActive (false);
-			//target2.SetActive (false);
-			cam.SetActive (false);
-		}
-			//switcher = true;
-		//if (switcher == true)
-	}
-	void setDense(){
-		dense = true;
-	}
-	void setSparse(){
-		sparse = true;
-	}
-}
-=======
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -121,4 +68,3 @@ public class Switcher3 : MonoBehaviour {
 		start = true;
 	}
 }
->>>>>>> 50c83f3ffaa352172676b9dbe54db6575f5e67b5

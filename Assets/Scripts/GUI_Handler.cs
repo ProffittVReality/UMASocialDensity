587d40bb1723a7e089f3415c7759ac6c54009db5
ProffittVReality <uvaperceptionlab@gmail.com>
2018-03-02 02:06:01 -0500

    Merge branch 'master' of https://github.com/ProffittVReality/UMASocialDensity

--- Assets/Scripts/GUI_Handler.cs
+++ Assets/Scripts/GUI_Handler.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
@@ -54,3 +55,61 @@ public class GUI_Handler : MonoBehaviour {
 	}
 
 }
+=======
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class GUI_Handler : MonoBehaviour {
+
+	public GameObject menu; //Assign canvas to this in inspector, make sure script is on EventHandler
+	public string hideShow; //Set to the key you want to press to open and close the GUI
+	public string FileName; //Title the file you want to export data to!  Will be saved in resources.
+
+	private bool isShowing;
+	public InputField raName, partic,exp,age,height,weight, time, other; 
+	//TODO: Ask around to see if there's a more efficient and elegant method to get references to all of the input fields!
+	public Toggle left, right;
+	public Dropdown sex;
+	public Button export;
+
+	// Use this for initialization
+	void Start () {
+		isShowing = true;
+		export.onClick.AddListener (exportData);
+		if(hideShow.Equals(""))
+			hideShow = "escape";
+		if (FileName.Equals (""))
+			FileName = "default";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (hideShow)) {
+			isShowing = !isShowing;
+			menu.SetActive (isShowing);
+		}
+	}
+
+	void exportData() {
+		string path = @"Assets\Data\" + FileName + ".txt";
+		string theTime = DateTime.Now.ToString ("hh:mm:ss");
+		string theDate = DateTime.Now.ToString ("d");
+		if (!File.Exists (path)) {
+			string header = "Time\tName\tP #\tExp #\tAge\tHeight\tWeight\tGender\tHand\tOther\r\n";
+			File.WriteAllText (path, header);
+		}
+		string hand = "";
+		if (left.isOn)
+			hand = hand + "D";
+		if (right.isOn)
+			hand = hand + "S";
+		string appendText = theTime+" "+theDate +"\t"+ raName.text +"\t"+ partic.text +"\t"+ exp.text +"\t"+ age.text +
+			"\t"+ height.text +"\t"+ weight.text +"\t"+ sex.captionText.text +"\t"+ hand +"\t"+ time.text + "\t" + other.text + "\r\n";
+		File.AppendAllText (path, appendText);
+	}
+
+}
+>>>>>>> 684e6f4ec9a14e4d9754edd344ebc1543c54fbd2
--- Assets/Switcher3.cs
+++ Assets/Switcher3.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+<<<<<<< HEAD
+﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -6,8 +7,6 @@ public class Switcher3 : MonoBehaviour {
 	public KeyCode method;
 	public GameObject target;
 	public GameObject target2;
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public bool dense = false;
 	public bool sparse = false;
 	public bool start = false;
@@ -21,25 +20,11 @@ public class Switcher3 : MonoBehaviour {
 		count = 0;
 		c = blackBlock.GetComponent<Renderer> ().material.color;
 		blackBlock.SetActive (false);
-
-=======
-=======
->>>>>>> parent of 898e409... outdoor scene update
-	//public bool switcher;
-
-
-	void Awake() {
-		//switcher = false;
-<<<<<<< HEAD
->>>>>>> parent of 898e409... outdoor scene update
-=======
->>>>>>> parent of 898e409... outdoor scene update
+			
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-<<<<<<< HEAD
 		if (start) {
 			if (count == 0) {
 				timeLeft = float.Parse (timer.text) * 60.0f;
@@ -74,31 +59,85 @@ public class Switcher3 : MonoBehaviour {
 	}
 	void setSparse(){
 		sparse = true;
-=======
->>>>>>> parent of 898e409... outdoor scene update
-=======
->>>>>>> parent of 898e409... outdoor scene update
 		if (Input.GetKeyDown(method))
 			//Debug.Log("key was pressed");
 			target.SetActive(true); 
 		if (target.active)
 			target2.SetActive(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
 	}
 	void exportStart(){
 		start = true;
-=======
-=======
->>>>>>> parent of 898e409... outdoor scene update
-
-
-			//switcher = true;
-		//if (switcher == true)
-			
-<<<<<<< HEAD
->>>>>>> parent of 898e409... outdoor scene update
-=======
->>>>>>> parent of 898e409... outdoor scene update
 	}
 }
+=======
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Switcher3 : MonoBehaviour {
+	public KeyCode method;
+	public GameObject target;
+	public GameObject target2;
+	public bool dense = false;
+	public bool sparse = false;
+	public bool start = false;
+	public InputField timer;
+	public float timeLeft;
+	public GameObject blackBlock;
+	public Color c;
+	int count;
+	void Awake() {
+		//switcher = false;
+		count = 0;
+		c = blackBlock.GetComponent<Renderer> ().material.color;
+		blackBlock.SetActive (false);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (start) {
+			if (count == 0) {
+				timeLeft = float.Parse (timer.text) * 60.0f;
+			}
+			count = 1;
+			if (dense) {
+				target.SetActive (true);
+				target2.SetActive (false);
+			} 
+			else if (sparse){
+				target2.SetActive (true);
+				target.SetActive (false);
+			}
+
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 6 && timeLeft > 5) {
+				blackBlock.SetActive (true);
+			}
+			if (timeLeft <= 5) {
+				c.a += Mathf.Clamp01 (Time.deltaTime / 5);
+				blackBlock.GetComponent<Renderer> ().material.color = c;
+			}
+			if (timeLeft <= 0) {
+				target.SetActive (false);
+				target2.SetActive (false);
+			}
+		}
+
+	}
+	void setDense(){
+		dense = true;
+	}
+	void setSparse(){
+		sparse = true;
+		if (Input.GetKeyDown(method))
+			//Debug.Log("key was pressed");
+			target.SetActive(true); 
+		if (target.active)
+			target2.SetActive(false);
+	}
+	void exportStart(){
+		start = true;
+	}
+}
+>>>>>>> 684e6f4ec9a14e4d9754edd344ebc1543c54fbd2
